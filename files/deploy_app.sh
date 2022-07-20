e1fdb7171d6419ac3ac2601ddedaf6ceaf64a78e
Erik Sabowski <airyk@sabowski.com>
2022-07-20 14:49:08 -0400

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Cat, I'm a kitty cat, and I dance dance dance! DANCE DANCE DANCE
+  Welcome to ${PREFIX}'s app. Cat, I'm a kitty cat, and I dance dance dance! DANCE DANCE DANCE. MORE!!!
   <!-- END -->
 
   </div>
